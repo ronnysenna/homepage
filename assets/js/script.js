d4d3f22203071b4e247e161b8c8a4a28a5b1633b
Ronny Senna <ronnysenna@yahoo.com>
2024-11-09 13:19:10 -0300

    add botão topo

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,17 @@ document.getElementById('darkModeToggle').addEventListener('click', function () 
     document.querySelector('footer').classList.toggle('dark-mode');
     this.classList.toggle('dark-mode');
 });
+
+// Exibir o botão ao rolar a página para baixo
+function subirParaInicio() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+window.onscroll = () => {
+    const botaoTopo = document.getElementById("botaoTopo");
+    if (window.scrollY > 200) { // Exibe o botão após rolar 200px
+        botaoTopo.style.display = "block";
+    } else {
+        botaoTopo.style.display = "none";
+    }
+};
